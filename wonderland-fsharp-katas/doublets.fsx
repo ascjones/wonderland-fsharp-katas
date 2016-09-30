9963199239a61e5ae93da78cb7c786c31fc09f36
Andrew Jones <andrew.jones@contrarius.co.uk>
2016-09-30 21:55:23 +0100

    initial attempt at doublets, failing 1 test

--- wonderland-fsharp-katas/doublets.fsx
+++ wonderland-fsharp-katas/doublets.fsx
@@ -1,15 +1,38 @@
 // See the file doublets.md for detailed information.
 
+open System
 open System.IO
 
 let wordsPath = Path.Combine (__SOURCE_DIRECTORY__,"resources","words.txt")
 let words = File.ReadAllLines wordsPath
 
 type Word = string
-let doublets (w1:Word,w2:Word) = [ "make me work" ]
+
+let areWordsLinked (w1: Word) (w2: Word) =
+    let matchingChars = 
+        [0 .. w1.Length - 1] 
+        |> List.fold (fun acc i -> if w1.[i] = w2.[i] then acc + 1 else acc) 0
+    matchingChars = w1.Length - 1
+
+let doublets (w1:Word,w2:Word) = 
+    if w1.Length = w2.Length then
+        let wordsOfLength = 
+            words 
+            |> Array.toList
+            |> List.filter (fun w -> w.Length = w1.Length)
+        let findNextWord (prevWord,currWord) = 
+            if currWord <> w2 then
+                wordsOfLength
+                |> List.tryFind (fun w -> w <> prevWord && areWordsLinked currWord w)
+                |> Option.map (fun w -> (currWord, (currWord, w)))
+            else None
+        List.append (Seq.unfold findNextWord (w1,w1) |> Seq.toList) [w2]
+    else []
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
+
+//areWordsLinked "head" "heal"
 
 let tests () =
 
