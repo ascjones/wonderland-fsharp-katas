5302d9291a88332c901a3854c599451ff60b3e51
Andrew Jones <ascjones@gmail.com>
2017-05-26 08:56:08 +0100

    implement alphabet cipher encode

--- wonderland-fsharp-katas/alphabet-cipher.fsx
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -1,10 +1,46 @@
 // See the file alphabet-cipher.md for detailed information.
 
+open System
+
 type Message = string
 type Keyword = string
 
+let charToInt (c: char) =
+    System.Convert.ToInt16 c
+
+let intToChar (i: int16) =
+    System.Convert.ToChar i
+
+let a =
+    let res = charToInt 'a'
+    printfn "a = %i" res
+    res
+
+let z = charToInt 'z'
+
+let toCharCode (c: char) =
+    charToInt c - a
+
+let toCharInts (s: string) =
+    s.ToCharArray() |> Array.map toCharCode
+
+let repeatKey (key: Keyword) (fillCount: int) =
+    [|0 .. (fillCount - 1)|]
+    |> Array.map (fun i ->
+        key.ToCharArray()
+        |> Array.item (i % key.Length)
+        |> toCharCode)
+
+let mapChars f (key: Keyword) (msg: string) =
+    Array.zip (toCharInts msg) (repeatKey key msg.Length)
+    |> Array.map f
+    |> String
+
 let encode (key:Keyword) (message:Message) : Message =
-    "encodeme"
+    let msgChars = message.ToCharArray()
+    let keyChars = key.ToCharArray()
+    mapChars (fun (msgCode, keyCode) ->
+        ((keyCode + msgCode) % 26s) + a |> intToChar) key message
 
 let decode (key:Keyword) (message:Message) : Message =
     "decodeme"
