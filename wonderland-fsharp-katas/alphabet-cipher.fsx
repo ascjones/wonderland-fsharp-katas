9d67cfff2e4efd3e69f9f4aedf1b4bda7cfd906c
Andrew Jones <ascjones@gmail.com>
2017-05-26 10:10:14 +0100

    refactor alphabet cipher encode/decode

--- wonderland-fsharp-katas/alphabet-cipher.fsx
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -6,16 +6,15 @@ type Message = string
 type Keyword = string
 
 let charToInt (c: char) =
-    System.Convert.ToInt16 c
-
-let intToChar (i: int16) =
-    System.Convert.ToChar i
+    Convert.ToInt16 c
 
 let a = charToInt 'a'
-let z = charToInt 'z'
 
 let toCharCode (c: char) =
     charToInt c - a
+
+let fromCharCode i =
+    Convert.ToChar (i + a)
 
 let toCharInts (s: string) =
     s.ToCharArray() |> Array.map toCharCode
@@ -29,17 +28,26 @@ let repeatKey (key: Keyword) (fillCount: int) =
 
 let mapChars f (key: Keyword) (msg: string) =
     Array.zip (toCharInts msg) (repeatKey key msg.Length)
-    |> Array.map (f >> intToChar)
+    |> Array.map (f >> fromCharCode)
     |> String
+
+// sample grid of chars mapped to integers, a = 0, z = 25
+//
+//  key char ->    0 1 2 3
+//              0  0 1 2 3
+//              1  1 2 3 0
+//              2  2 3 0 1
+//              3  3 0 1 2
+//  msg char -- ^
 
 let encode (key:Keyword) (message:Message) : Message =
     let encodeChar (msgCode, keyCode) =
-        ((keyCode + msgCode) % 26s) + a
+        ((keyCode + msgCode) % 26s)
     mapChars encodeChar key message
 
 let decode (key:Keyword) (message:Message) : Message =
     let decodeChar (msgCode, keyCode) =
-        ((msgCode - keyCode + 26s) % 26s) + a
+        ((msgCode - keyCode + 26s) % 26s)
     mapChars decodeChar key message
 
 let decipher (cipher:Message) (message:Message) : Keyword =
