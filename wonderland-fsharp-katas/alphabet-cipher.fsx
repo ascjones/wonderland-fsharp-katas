e4953388cbe754b973c1a3f8e6710458b6c4d990
Andrew Jones <ascjones@gmail.com>
2017-05-26 11:04:29 +0100

    implement alphabet cipher decipher

--- wonderland-fsharp-katas/alphabet-cipher.fsx
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -14,22 +14,22 @@ let toCharCode (c: char) =
     charToInt c - a
 
 let fromCharCode i =
-    Convert.ToChar (i + a)
+    Convert.ToChar ((i % 26s) + a)
 
 let toCharInts (s: string) =
     s.ToCharArray() |> Array.map toCharCode
 
-let repeatKey (key: Keyword) (fillCount: int) =
-    [|0 .. (fillCount - 1)|]
+let repeatKey (key: Keyword) (msg: Message) =
+    [|0 .. (msg.Length - 1)|]
     |> Array.map (fun i ->
         key.ToCharArray()
-        |> Array.item (i % key.Length)
-        |> toCharCode)
+        |> Array.item (i % key.Length))
 
 let mapChars f (key: Keyword) (msg: string) =
-    Array.zip (toCharInts msg) (repeatKey key msg.Length)
+    repeatKey key msg
+    |> Array.map toCharCode
+    |> Array.zip (toCharInts msg)
     |> Array.map (f >> fromCharCode)
-    |> String
 
 // sample grid of chars mapped to integers, a = 0, z = 25
 //
@@ -42,16 +42,26 @@ let mapChars f (key: Keyword) (msg: string) =
 
 let encode (key:Keyword) (message:Message) : Message =
     let encodeChar (msgCode, keyCode) =
-        ((keyCode + msgCode) % 26s)
-    mapChars encodeChar key message
+        keyCode + msgCode
+    mapChars encodeChar key message |> String
 
 let decode (key:Keyword) (message:Message) : Message =
     let decodeChar (msgCode, keyCode) =
-        ((msgCode - keyCode + 26s) % 26s)
-    mapChars decodeChar key message
+        msgCode - keyCode + 26s
+    mapChars decodeChar key message |> String
 
-let decipher (cipher:Message) (message:Message) : Keyword =
-    "decypherme"
+let decipher (cipher:Message) (msg:Message) : Keyword =
+    let decipherChar (cipherCode, msgCode) =
+        msgCode - cipherCode + 26s
+    let repeatedKey =
+        mapChars decipherChar cipher msg // todo: extract mapChars without key repeat
+    let rec extractKey acc = function
+        | [] -> acc
+        | x :: xs ->
+            let key = Array.append acc [|x|]
+            if repeatKey (String key) msg = repeatedKey then key
+            else extractKey key xs
+    extractKey [||] (repeatedKey |> Array.toList) |> String
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
